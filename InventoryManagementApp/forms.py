93f9a9cb8a5f27d4c5d6dcc036c8eccb2e6acb5e
RakhiRanglani <rakhiranglani222@gmail.com>
2022-04-10 14:13:57 +0100

    "Updated home page , static file was not loading error fixed and migration of database is successful"

--- InventoryManagementApp/forms.py
+++ InventoryManagementApp/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 
+
 class EntryForm(forms.Form):
     bought = forms.IntegerField(
-    label='Enter tomatoes bought today: '
+        label='Enter tomatoes bought today: '
     )
+
+
 class StatsForm(forms.Form):
     months = forms.IntegerField(
-    label='Predict estimated order how many months ahead?'
+        label='Predict estimated order how many months ahead?'
     )
--- InventoryManagementApp/models.py
+++ InventoryManagementApp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
 class Food(models.Model):
     name = models.CharField(max_length=100)
     amount = models.CharField(max_length=100)
     expiration = models.CharField(max_length=100)
-    address = models.CharField(max_length=1000,default='Carnegie Mellon')
-    key = models.CharField(max_length=1200,default='asdf')
-    phone=models.CharField(max_length=15,default='111-111-1111')
+    address = models.CharField(max_length=1000, default='Carnegie Mellon')
+    key = models.CharField(max_length=1200, default='asdf')
+    phone = PhoneNumberField(unique = True, null = False, blank = False)
